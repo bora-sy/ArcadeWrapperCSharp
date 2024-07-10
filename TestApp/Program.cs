ebcf47c85243c1ccced9d14f9387c4a1981386dc
Bora <89869983+BoRa-SY@users.noreply.github.com>
2024-07-10 23:31:33 +0300

    Created test app

---
+++ TestApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using HackclubArcadeAPIWrapper.HTTPRequest;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        static void LogResponse(HTTPRequestResponse resp)
+        {
+            if (resp.SendSuccess)
+            {
+                Console.WriteLine("Request suc");
+                Console.WriteLine($"Status Code: {resp.StatusCode}");
+                Console.WriteLine($"Response: {resp.StringContent}");
+            }
+            else
+            {
+                Console.WriteLine("Request failed");
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            var sender = new HTTPRequestSender("{api key}");
+
+            Console.WriteLine("Sending Request");
+            var resp = await sender.GET("https://hackhour.hackclub.com/api/session/{slack member id}");
+            Console.WriteLine("Request Sent");
+
+
+            LogResponse(resp);
+        }
+    }
+
+}
